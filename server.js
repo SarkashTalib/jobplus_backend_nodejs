973093f099f253bcd3867d02a285c6b19de9735a
Sarkash Talib <sarkash.talib@gmail.com>
2022-11-27 23:14:10 +0000

    configuring server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
