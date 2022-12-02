450433d0f69ef4aa7a83e0996e807527358fc3f5
Sarkash Talib <sarkash.talib@gmail.com>
2022-12-02 09:40:57 +0000

    fixed database connection bug

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_URL,
+  user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
