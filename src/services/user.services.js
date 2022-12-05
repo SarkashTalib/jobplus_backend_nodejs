dc274d52edeeee5ed1c53e00fd6466b7f9a78859
Sarkash Talib <sarkash.talib@gmail.com>
2022-12-05 13:05:22 +0000

    user validation

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -5,9 +5,23 @@ const bcrypt = require('bcrypt');
 exports.createUser = async (body) => {
   const { first_name, last_name, email, password, confirm_password } = body;
 
+  // check if email already exists
+  const checkEmail = await db.query('SELECT * FROM "users" WHERE email = $1', [email]);
+  if (checkEmail.rows.length > 0) {
+    throw new Error('Email already exists!');
+  }
+
+  // check if passwords match
+  if (password !== confirm_password) {
+    throw new Error('Passwords do not match!');
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const { rows } = await db.query( 
     'INSERT INTO "users" (first_name, last_name, email, password) VALUES ($1, $2, $3, $4) RETURNING *',
-    [first_name, last_name, email, password]
+    [first_name, last_name, email, hashedPassword]
   );
 
   return rows;
